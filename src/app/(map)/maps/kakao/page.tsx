1a59f189169ffe0ef0ed235b319d6615cc1faf22
Owen <owen1127@naver.com>
2024-07-13 17:26:18 +0900

    feat : page map

--- src/app/(map)/maps/kakao/page.tsx
+++ src/app/(map)/maps/kakao/page.tsx
@@ -1,5 +1,19 @@
-const Page = () => {
-  return <div></div>;
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import Script from 'next/script';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+const MapPage = () => {
+  const loadKakaoMap = () => {};
+  return <div css={MapStyle}></div>;
 };
 
-export default Page;
+export default MapPage;
+const MapStyle = css`
+  width: 100vw;
+`;
