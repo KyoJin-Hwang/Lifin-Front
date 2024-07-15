f06ba49e136a8569b8ef3752e467bd3aa3d27c64
KyoJin-Hwang <owen1127@naver.com>
2024-07-15 00:49:07 +0000

    feat : meta 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './global.css';
 import Navigation from '@/components/navigation/navigation';
 export const metadata: Metadata = {
   title: 'Lifin',
-  description: '일상에 핀을 더해봐 라이핀!',
+  description: '일상에 핀을 더해보자 라이핀!',
 };
 
 export default function RootLayout({
