e2b681d0eaa6e6ebee67b341f1cbbec49d7d569a
KyoJin-Hwang <owen1127@naver.com>
2024-07-15 10:26:17 +0000

    fix : meta데이터 이모지 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './global.css';
 import Navigation from '@/components/navigation/navigation';
 export const metadata: Metadata = {
   title: 'Lifin',
-  description: '일상에 핀을 더해보자 라이핀!',
+  description: '일상에 핀을 더해보자 라이핀📌',
 };
 
 export default function RootLayout({
